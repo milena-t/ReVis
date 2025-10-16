a71e41d9ecff7d180524e499d1cfc6002fcff097
milena-t <milena.trabert@t-online.de>
2025-10-16 19:14:31 +0200

    bisschen code auslagern weil das echt lang wurde

--- src/ReVis/ReVis_transcript_surroundings.py
+++ src/ReVis/ReVis_transcript_surroundings.py
@@ -7,6 +7,7 @@ annotated on that base
 import parse_gff as gff
 import parse_orthogroups as OGs
 import make_transcript_surrounds_table as tr_surrounds
+import confidence_interval_surroundings as CI
 
 import numpy as np
 import sys
@@ -17,9 +18,6 @@ from matplotlib.lines import Line2D
 import time
 import argparse
 
-from sklearn.preprocessing import PolynomialFeatures
-import statsmodels.api as sm
-from statsmodels.sandbox.regression.predstd import wls_prediction_std
 
 def parse_args():
     # Create the parser
@@ -336,192 +334,6 @@ def read_transcript_IDs_csv(filepath:str)->list:
 
 
 
-def plot_confidence_intervals(before_filepath:str, after_filepath:str, sig_transcripts:int, general_legend_names = False, all_before_filepath:str = "", all_after_filepath:str = "", all_transcripts:int = 0, filename = "cumulative_repeat_presence_around_transcripts_95_perc_CI", modelstats_filename = "pol_reg_sum.txt", legend = True, plot_white_bg = False):
-    """
-    make a separate plot for each TE category with the foreground/background and before/after lines in it 
-    do the polynomial regressions and see the 95% confidence intervals
-    I followed this tutorial: https://ostwalprasad.github.io/machine-learning/Polynomial-Regression-using-statsmodel.html
-    """
-    plot_transparent_bg = True
-    if plot_white_bg:
-        plot_transparent_bg = False
-    
-    before_dict = gff.read_dict_from_file(before_filepath)
-    before_dict = { key : [int(v)/sig_transcripts*100 for v in value] for key, value in before_dict.items()}
-    # show percentages of they are too high
-    for key, value in before_dict.items():
-        for v in value:
-            v_ = int(v)
-            perc = v_/sig_transcripts*100
-            if perc>150:
-                print(f"{key} : \t {v_}/{sig_transcripts} * 100 = {perc:.2f}%")
-
-    after_dict = gff.read_dict_from_file(after_filepath)
-    after_dict = { key : [int(v)/sig_transcripts*100 for v in value] for key, value in after_dict.items()}
-
-    all_before_dict = {}
-    all_after_dict = {}
-    if all_before_filepath !="" and all_after_filepath !="" :
-        
-        all_before_dict = gff.read_dict_from_file(all_before_filepath)
-        all_before_dict = { key : [int(v)/all_transcripts*100 for v in value] for key, value in all_before_dict.items()}
-        all_after_dict = gff.read_dict_from_file(all_after_filepath)
-        all_after_dict = { key : [int(v)/all_transcripts*100 for v in value] for key, value in all_after_dict.items()}
-        
-    colors = {
-        'Unknown' : "#C1C1C1" , # light grey
-        # orange
-        'DNA' : "#FF9000" , # Princeton orange
-        # green
-        'LTR' : "#6E8448" , # reseda green
-        'RC' : "#8EA861" , # asparagus 
-        # red
-        'tRNA' : "#C14953" , # bittersweet shimmer
-        'rRNA' : "#D0767E" , # old rose
-        'snRNA' : "#7A2A30" , # wine
-        # blue 
-        'LINE' : "#3476AD" , # UCLA blue
-        'SINE': "#72A8D5" , # ruddy blue
-        'SINE?': "#72A8D5" , # ruddy blue
-        # '' : "#2A618D" , #lapis lazuli
-        # dark red-brown
-        'Low_complexity' : "#3A3335" , # Jet 
-        'Satellite' : "#564D4F" , #Wenge 
-        'Simple_repeat' : "#827376" , #Taupe gray
-    }
-
-    fs = 25 # set font size
-
-    rep_classes = list(before_dict.keys())
-    num_bp = len(before_dict[rep_classes[0]])
-    x_before = range(-num_bp, 0)
-    x_after = range(num_bp)
-
-    for rep_class in rep_classes:
-        if legend:
-            fig, ax = plt.subplots(1, 1, figsize=(23, 10))
-        else:
-            fig, ax = plt.subplots(1, 1, figsize=(21, 12))
-
-        species = gff.split_at_second_occurrence(before_filepath.split("/")[-1])
-        species = species.replace("_", ". ")
-
-        max_percentage = 0
-
-        max_before = max(before_dict[rep_class])
-        max_after = max(after_dict[rep_class])
-        if max_before>max_percentage:
-            max_percentage=max_before
-        if max_after>max_percentage:
-            max_percentage=max_after
-
-        # make polynomial features
-        polynomial_features= PolynomialFeatures(degree = 4)
-        ## I tried a few degrees but 4 looks the most reasonable
-        x_before_reshape = np.array(x_before).reshape(-1,1)
-        # print(f"after reshaping: {x_before_reshape.shape}")
-        p_before = polynomial_features.fit_transform(x_before_reshape)
-        x_after_reshape = np.array(x_after).reshape(-1,1)
-        # print(f"after reshaping: {x_after_reshape.shape}")
-        p_after = polynomial_features.fit_transform(x_after_reshape)
-
-        # model polynomial regression
-        before_model = sm.WLS(before_dict[rep_class], p_before, weights=np.full_like(before_dict[rep_class], num_sig_transcripts)).fit()
-        all_before_model = sm.WLS(all_before_dict[rep_class], p_before, weights=np.full_like(all_before_dict[rep_class], num_all_transcripts)).fit()
-        after_model = sm.WLS(after_dict[rep_class], p_after, weights=np.full_like(after_dict[rep_class], num_sig_transcripts)).fit()
-        all_after_model = sm.WLS(all_after_dict[rep_class], p_after, weights=np.full_like(all_after_dict[rep_class], num_all_transcripts)).fit()
-
-        ## calculate predicted polynomial
-        before_ypred = before_model.predict(p_before) 
-        all_before_ypred = all_before_model.predict(p_before) 
-        after_ypred = after_model.predict(p_after) 
-        all_after_ypred = all_after_model.predict(p_after) 
-
-        ## calculate predicted confidence interval
-        _, upper_before_model,lower_before_model = wls_prediction_std(before_model)
-        _, upper_all_before_model,lower_all_before_model = wls_prediction_std(all_before_model)
-        _, upper_after_model,lower_after_model = wls_prediction_std(after_model)
-        _, upper_all_after_model,lower_all_after_model = wls_prediction_std(all_after_model)
-
-        rep_label = rep_class.replace("_", " ")
-        ax.plot(x_before_reshape, before_dict[rep_class], color = colors[rep_class], linewidth=2)
-        ax.plot(x_after_reshape, after_dict[rep_class], color = colors[rep_class], linewidth=2)
-        ax.plot(x_before_reshape, before_ypred, label = f"{rep_label} pol. reg.\nforeground", color = colors[rep_class], linewidth = 3, linestyle = (0, (5, 2)))
-        ax.plot(x_after_reshape, after_ypred, color = colors[rep_class], linewidth=3, linestyle = (0, (5, 2)))
-        ax.fill_between(x_before, upper_before_model,lower_before_model, color=colors[rep_class], alpha = 0.25)
-        ax.fill_between(x_after, upper_after_model,lower_after_model, color=colors[rep_class], alpha = 0.25)
-        
-        if all_before_dict !={} and all_after_dict !={} and all_transcripts!=0:
-            max_before = max(all_before_dict[rep_class])
-            max_after = max(all_after_dict[rep_class])
-            if max_before>max_percentage:
-                max_percentage=max_before
-            if max_after>max_percentage:
-                max_percentage=max_after
-
-            ax.plot(x_before, all_before_dict[rep_class], color = colors[rep_class], linestyle = (0, (1, 10)), linewidth = 2)                    
-            ax.plot(x_after, all_after_dict[rep_class], color = colors[rep_class], linestyle = (0, (1, 10)), linewidth = 2)   
-            ax.plot(x_before, all_before_ypred, color = colors[rep_class],label = f"{rep_label} pol. reg.\nbackground", linewidth=1, linestyle = (0, (5, 2)))
-            ax.plot(x_after, all_after_ypred, color = colors[rep_class], linewidth=1, linestyle = (0, (5, 2)))
-            ax.fill_between(x_before, upper_all_before_model,lower_all_before_model, color=colors[rep_class], alpha = 0.25)
-            ax.fill_between(x_after, upper_all_after_model,lower_all_after_model, color=colors[rep_class], alpha = 0.25, label = "confidence\ninterval")           
-
-        if True: ## print model output
-            o = sys.stdout
-            with open(f'{modelstats_filename}_{rep_class}.txt', 'w') as f:
-                sys.stdout = f
-                print(f"Species: {species} \nsummary of statistical models from polynomial regression for repeat class: {rep_label}")
-                print(f"\n############## before transcript foreground model")
-                print(before_model.summary())
-                print(f"\n\n############## before transcript background model")
-                print(all_before_model.summary())
-                print(f"\n\n############## after transcript foreground model")
-                print(after_model.summary())
-                print(f"\n\n############## after transcript background model")
-                print(all_after_model.summary())
-            sys.stdout = o
-
-        max_percentage=int(max_percentage*1.3)
-        if max_percentage == 0 or max_percentage>100:
-            max_percentage= 100
-
-        plt.vlines(x= 0, ymin=0, ymax=max_percentage, colors="#000000", linestyles="dashed", label="transcript border", linewidth=3)
-        plt.xticks(range(-num_bp, num_bp+1, int(num_bp/5)), fontsize = fs)
-        plt.yticks(range(0, max_percentage+1, 10), fontsize = fs)
-
-        if legend:
-            # plot color legend
-            ax.set_xlim([-num_bp, num_bp*1.55])
-            legend_colors = ax.legend(loc = "center right", fontsize = fs)
-            plt.gca().add_artist(legend_colors)
-            # plt.title(f"{species} transcript surroundings {num_bp} bp up and downstream \n({num_sig_transcripts} significant transcripts of {all_transcripts} in CAFE analysis)", fontsize = fs*1.25)
-            
-        # plot dotted/bold legend
-        solid = Line2D([0], [0], color='black', linestyle='-', linewidth=2)
-        dotted = Line2D([0], [0], color='black', linestyle=':', linewidth=2)
-        handles = [solid, dotted]
-        labels = []
-        if general_legend_names:
-            labels.append(f"foreground transcripts ({num_sig_transcripts})")
-            labels.append(f"background transcripts ({all_transcripts})")
-        else:
-            labels.append(f"significant transcripts ({num_sig_transcripts})")
-            labels.append(f"all CAFE transcripts ({all_transcripts})")
-        plt.legend(handles, labels, loc = "upper left", fontsize = fs)
-
-        plt.title(f"{species} transcript surroundings {num_bp} bp up and downstream\nrepeat category: {rep_label} with polynomial regression and 95% confidence interval", fontsize = fs*1.25)
-        plt.xlabel(f"basepairs upstream and downstream from transcript", fontsize = fs)
-
-        plt.ylabel(f"percent of transcripts in which this base is a repeat", fontsize = fs)
-        ax.yaxis.set_major_formatter(FuncFormatter(lambda x, pos: '' if x > 99 or x<1 else f'{int(x)}%'))
-        
-        plt.tight_layout()
-        filename = f"{filename}_{rep_class}.png"
-        plt.savefig(filename, dpi = 300, transparent = plot_transparent_bg)
-        plt.close(fig)
-        print(f"\t * repeat category {rep_label} \t--> Figure saved as: {filename}")
-
-
 
 
 
@@ -644,7 +456,7 @@ if __name__ == "__main__":
     if verbose:
         # print(f"\n  * plot confidence intervals for {species}")
         print(f"\n  * plot polynomial regression for all categories individually...")
-    plot_confidence_intervals(before_filepath = sig_before_transcript, after_filepath=sig_after_transcript, sig_transcripts = num_sig_transcripts, general_legend_names=args.compute_tables_from_list, all_before_filepath=all_before_transcript, all_after_filepath=all_after_transcript, all_transcripts=num_all_transcripts, filename=f"{args.out_dir}{species}_cumulative_repeat_presence_around_transcripts_95percent_confidence_interval", modelstats_filename =f"{args.out_dir}{species}_polynomial_regression_summary", legend=plot_legend, plot_white_bg=args.plot_white_background)
+    CI.plot_confidence_intervals(before_filepath = sig_before_transcript, after_filepath=sig_after_transcript, num_sig_transcripts = num_sig_transcripts, num_all_transcripts = num_all_transcripts, general_legend_names=args.compute_tables_from_list, all_before_filepath=all_before_transcript, all_after_filepath=all_after_transcript, all_transcripts=num_all_transcripts, filename=f"{args.out_dir}{species}_cumulative_repeat_presence_around_transcripts_95percent_confidence_interval", modelstats_filename =f"{args.out_dir}{species}_polynomial_regression_summary", legend=plot_legend, plot_white_bg=args.plot_white_background)
 
 
 

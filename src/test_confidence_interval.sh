3d008ed37fea36bb7d5d49218f849251db77ab99
milena-t <milena.trabert@t-online.de>
2025-11-06 23:00:59 +0100

    more testing

--- src/test_confidence_interval.sh
+++ src/test_confidence_interval.sh
@@ -16,9 +16,9 @@ python3 /Users/${U_NAME}/work/PhD_code/ReVis/src/ReVis/ReVis_transcript_surround
     --all_after_table ${REP_TABLES}/${SPECIES}_cumulative_repeats_after_all_transcripts.txt \
     --sig_after_table ${REP_TABLES}/${SPECIES}_cumulative_repeats_after_sig_transcripts_90th_GF_size_percentile.txt \
     --num_transcripts ${REP_TABLES}/${SPECIES}_transcript_numbers.txt \
-    --polreg_win_smooth 250 \
-    --overlapping_windows \
+    --polreg_fourier_denoise \
     --species_name $SPECIES \
     --verbose
 done
-    # --polreg_fourier_denoise \
+    # --overlapping_windows \
+    # --polreg_win_smooth 250 \

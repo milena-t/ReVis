b471dddb24a41d0c8fc841e2f8917d11b74c6b7f
milena-t <milena.trabert@t-online.de>
2026-02-28 22:13:18 +0100

    make enrichment analysis

--- paper_analysis/ReVis_GO_transcripts.sh
+++ paper_analysis/ReVis_GO_transcripts.sh
@@ -11,17 +11,17 @@
 source /proj/naiss2023-6-65/Milena/python_venvs/venv/bin/activate
 
 REVIS_PATH=/proj/naiss2023-6-65/Milena/ReVis_paper/ReVis/src/ReVis/
-ASSEMBLY_DIR=/proj/naiss2023-6-65/Milena/ReVis_paper/Nvit_analysis/assembly/
-ANNOTATION_DIR=/proj/naiss2023-6-65/Milena/ReVis_paper/Nvit_analysis/annotation/
+ASSEMBLY_DIR=/proj/naiss2023-6-65/Milena/ReVis_paper/Brsri_analysis/assembly/
+ANNOTATION_DIR=/proj/naiss2023-6-65/Milena/ReVis_paper/Brsri_analysis/annotation/
 
 python3 ${REVIS_PATH}ReVis_transcript_surroundings.py \
     --compute_tables_from_list \
-    --out_dir /proj/naiss2023-6-65/Milena/ReVis_paper/Nvit_analysis/ReVis_plots \
-    --masker_outfile ${ASSEMBLY_DIR}GCF_009193385.2_Nvit_psr_1.1_genomic_short_headers.fna.out \
-    --annotation_gff ${ANNOTATION_DIR}GCF_009193385.2_Nvit_psr_1.1_genomic_isoform_filtered.gff \
-    --all_list /proj/naiss2023-6-65/Milena/ReVis_paper/ReVis/paper_analysis/Nvit_background_transcripts_list.txt \
-    --sig_list /proj/naiss2023-6-65/Milena/ReVis_paper/ReVis/paper_analysis/Nvit_foreground_transcripts_list.txt \
-    --species_name N_vitripennis \
+    --out_dir /proj/naiss2023-6-65/Milena/ReVis_paper/Brsri_analysis/ReVis_plots \
+    --masker_outfile ${ASSEMBLY_DIR}GCF_032445375.1_Brsri_v3_genomic.fna.out \
+    --annotation_gff ${ANNOTATION_DIR}GCF_032445375.1_Brsri_v3_genomic_isoform_filtered.gff \
+    --all_list /proj/naiss2023-6-65/Milena/ReVis_paper/Brsri_analysis/Brsri_Revis_enrichment_analysisBrsri_background_transcripts_list.txt \
+    --sig_list /proj/naiss2023-6-65/Milena/ReVis_paper/Brsri_analysis/Brsri_Revis_enrichment_analysisBrsri_foreground_transcripts_list.txt \
+    --species_name B_rossius_r \
     --polreg_fourier_denoise \
     --bp 10000 \
     --verbose

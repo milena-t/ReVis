4f532a3b4581c16ac9c63556da4770043ad9dafa
milena-t <milena.trabert@t-online.de>
2026-03-12 15:44:30 +0100

    test random

--- paper_analysis/orphan_genes_test/Cmac_orphans.sh
+++ paper_analysis/orphan_genes_test/Cmac_orphans.sh
@@ -18,7 +18,9 @@ ANNOTATION_DIR=/proj/naiss2023-6-65/Milena/annotation_pipeline/only_orthodb_anno
 # awk -F'\t' '!/^#/{v=$3; sub(/^C_maculatus_/,"",v); sub(/_1$/,"",v); printf "%s%s",(c++?",":""),v} END{print ""}' /proj/naiss2023-6-65/Milena/annotation_pipeline/busco/busco2/BUSCO_orthoDB_annotation/C_maculatus_filtered_proteinfasta.fa/run_arthropoda_odb10/full_table.tsv > /proj/naiss2023-6-65/Milena/ReVis_paper/test_orphans/Cmac_BUSCOs.txt
 ## make orphan list with awk
 # awk '!/^#/{sub(/^C_maculatus_/,""); sub(/_1$/,""); printf "%s%s",(c++?",":""),$0} END{print ""}' /proj/naiss2023-6-65/Ashwath/mas_thesis/results/final_orphans_2.0/C_maculatus_e1e-1_cov0_bits0/C_maculatus_true_orphans.ids.txt > /proj/naiss2023-6-65/Milena/ReVis_paper/test_orphans/Cmac_orphans.txt
-
+## make random sample 
+# for i in random.sample(range(1,30000),1000):
+#     print(f"g{i}.t1")
 
 python3 ${REVIS_PATH}ReVis_transcript_surroundings.py \
     --compute_tables_from_list \
@@ -26,8 +28,8 @@ python3 ${REVIS_PATH}ReVis_transcript_surroundings.py \
     --masker_outfile ${ASSEMBLY_DIR}assembly_genomic.fna.out \
     --annotation_gff ${ANNOTATION_DIR}braker_isoform_filtered.gff \
     --all_list /proj/naiss2023-6-65/Milena/ReVis_paper/test_orphans/Cmac_BUSCOs.txt \
-    --sig_list /proj/naiss2023-6-65/Milena/ReVis_paper/test_orphans/Cmac_orphans.txt \
-    --species_name C_maculatus \
+    --sig_list /proj/naiss2023-6-65/Milena/ReVis_paper/test_orphans/Cmac_random.txt \
+    --species_name RandomC_maculatus \
     --polreg_fourier_denoise \
-    --bp 50000 \
+    --bp 10000 \
     --verbose

22dfd87e98d522180d5b4243d6a4c25a1a780de5
milena-t <milena.trabert@t-online.de>
2026-06-05 10:59:50 +0200

    update new dir on uppmax

--- paper_analysis/ReVis_GO_transcripts.sh
+++ paper_analysis/ReVis_GO_transcripts.sh
@@ -8,19 +8,19 @@
 #SBATCH --mail-user milena.trabert@ebc.uu.se
 
 ## don't load the biopython module if using the venv!
-source /proj/naiss2023-6-65/Milena/python_venvs/venv/bin/activate
+source /proj/coleoptera-genomics-2025/snic2021-6-30/Milena/python_venvs/venv/bin/activate
 
-REVIS_PATH=/proj/naiss2023-6-65/Milena/ReVis_paper/ReVis/src/ReVis/
-ASSEMBLY_DIR=/proj/naiss2023-6-65/Milena/ReVis_paper/Brsri_analysis/assembly/
-ANNOTATION_DIR=/proj/naiss2023-6-65/Milena/ReVis_paper/Brsri_analysis/annotation/
+REVIS_PATH=/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/ReVis_paper/ReVis/src/ReVis/
+ASSEMBLY_DIR=/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/ReVis_paper/Brsri_analysis/assembly/
+ANNOTATION_DIR=/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/ReVis_paper/Brsri_analysis/annotation/
 
 python3 ${REVIS_PATH}ReVis_transcript_surroundings.py \
     --compute_tables_from_list \
-    --out_dir /proj/naiss2023-6-65/Milena/ReVis_paper/Brsri_analysis/ReVis_plots \
+    --out_dir /proj/coleoptera-genomics-2025/snic2021-6-30/Milena/ReVis_paper/Brsri_analysis/ReVis_plots \
     --masker_outfile ${ASSEMBLY_DIR}GCF_032445375.1_Brsri_v3_genomic.fna.out \
     --annotation_gff ${ANNOTATION_DIR}GCF_032445375.1_Brsri_v3_genomic_isoform_filtered.gff \
-    --all_list /proj/naiss2023-6-65/Milena/ReVis_paper/Brsri_analysis/Brsri_Revis_enrichment_analysisBrsri_background_transcripts_list.txt \
-    --sig_list /proj/naiss2023-6-65/Milena/ReVis_paper/Brsri_analysis/Brsri_Revis_enrichment_analysisBrsri_foreground_transcripts_list.txt \
+    --all_list /proj/coleoptera-genomics-2025/snic2021-6-30/Milena/ReVis_paper/Brsri_analysis/Brsri_Revis_enrichment_analysisBrsri_background_transcripts_list.txt \
+    --sig_list /proj/coleoptera-genomics-2025/snic2021-6-30/Milena/ReVis_paper/Brsri_analysis/Brsri_Revis_enrichment_analysisBrsri_foreground_transcripts_list.txt \
     --species_name B_rossius_r \
     --polreg_fourier_denoise \
     --bp 10000 \
--- paper_analysis/isoform_filter_gff.sh
+++ paper_analysis/isoform_filter_gff.sh
@@ -11,16 +11,16 @@
 
 module load AGAT/1.6.1-GCCcore-13.3.0
 
-# ANNOT_GFF=/proj/naiss2023-6-65/Milena/ReVis_paper/Nvit_analysis/annotation/GCF_009193385.2_Nvit_psr_1.1_genomic.gff
-# FILTERED_GTF=/proj/naiss2023-6-65/Milena/ReVis_paper/Nvit_analysis/annotation/GCF_009193385.2_Nvit_psr_1.1_genomic_isoform_filtered.gff
-# ASSEMBLY=/proj/naiss2023-6-65/Milena/ReVis_paper/Nvit_analysis/assembly/GCF_009193385.2_Nvit_psr_1.1_genomic_short_headers.fna
+# ANNOT_GFF=/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/ReVis_paper/Nvit_analysis/annotation/GCF_009193385.2_Nvit_psr_1.1_genomic.gff
+# FILTERED_GTF=/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/ReVis_paper/Nvit_analysis/annotation/GCF_009193385.2_Nvit_psr_1.1_genomic_isoform_filtered.gff
+# ASSEMBLY=/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/ReVis_paper/Nvit_analysis/assembly/GCF_009193385.2_Nvit_psr_1.1_genomic_short_headers.fna
 
-ANNOT_GFF=/proj/naiss2023-6-65/Milena/ReVis_paper/Brsri_analysis/annotation/GCF_032445375.1_Brsri_v3_genomic.gff
-FILTERED_GTF=/proj/naiss2023-6-65/Milena/ReVis_paper/Brsri_analysis/annotation/GCF_032445375.1_Brsri_v3_genomic_isoform_filtered.gff
-ASSEMBLY=/proj/naiss2023-6-65/Milena/ReVis_paper/Brsri_analysis/assembly/GCF_032445375.1_Brsri_v3_genomic.fna.masked
+ANNOT_GFF=/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/ReVis_paper/Brsri_analysis/annotation/GCF_032445375.1_Brsri_v3_genomic.gff
+FILTERED_GTF=/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/ReVis_paper/Brsri_analysis/annotation/GCF_032445375.1_Brsri_v3_genomic_isoform_filtered.gff
+ASSEMBLY=/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/ReVis_paper/Brsri_analysis/assembly/GCF_032445375.1_Brsri_v3_genomic.fna.masked
 
 # interactive -A uppmax2026-1-8 -t 5:00:00
-perl /proj/naiss2023-6-65/Milena/gene_family_analysis/filter_longest_isoform/agat_sp_keep_longest_isoform.pl --gff $ANNOT_GFF -o $FILTERED_GTF
+perl /proj/coleoptera-genomics-2025/snic2021-6-30/Milena/gene_family_analysis/filter_longest_isoform/agat_sp_keep_longest_isoform.pl --gff $ANNOT_GFF -o $FILTERED_GTF
 ANNOT_GFF_ID=${ANNOT_GFF_RAW}_AGAT_ID.gff
 agat_sp_manage_IDs.pl --gff $FILTERED_GTF -o $ANNOT_GFF_ID
 
